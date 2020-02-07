50e560341cbab33c655524e1ee3ea910b7d70487
rajasrk1993 <rajasravikiran@gmail.com>
2020-02-07 15:09:32 +0530

    Added Files

---
+++ GetOSData.java
@@ -0,0 +1,13 @@
+
+public class GetOSData {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String os=	System.getProperty("os.name");
+	
+	System.out.println(os);
+	
+	}
+
+}
